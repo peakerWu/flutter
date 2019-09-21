e77ea566d122a5b28b75600b6d764ecfae55d53e
wll <wll@wlldeMacBook-Pro.local>
2019-09-21 21:05:03 +0800

    update GridView Builder

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -2,6 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/model/post.dart';
 
 class ViewDemo extends StatelessWidget {
+  Widget _gridItemuilder(BuildContext context, int index) {
+    return Stack(
+      children: <Widget>[
+        SizedBox.expand(
+          child: Image.network(
+            posts[index].imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned(
+          left: 8.0,
+          bottom: 8.0,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                posts[index].title,
+                style: TextStyle(fontSize: 12.0, color: Colors.white),
+              ),
+              Text(
+                posts[index].author,
+                style: TextStyle(fontSize: 12.0, color: Colors.white),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: posts.length,
+      itemBuilder: _gridItemuilder,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, mainAxisSpacing: 8.0, crossAxisSpacing: 8.0),
+    );
+  }
+}
+
+class GridViewDemo extends StatelessWidget {
   List<Widget> _buildTiles(int length) {
     return List.generate(length, (int index) {
       return Container(
